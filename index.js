11e08f5f04de578ef69e2782c9d87f6d1b8ebacb
Graham Letorney <gletorney@shutterstock.com>
2019-06-20 16:06:17 -0400

    websocket update
    WebSocket broadcasting to all connected WebSocket clients, including itself.

--- index.js
+++ index.js
@@ -14,15 +14,12 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
