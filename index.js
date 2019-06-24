3215468b24b730a987d3b3d5f2f9fc739cdf5e03
Graham Letorney <gletorney@shutterstock.com>
2019-06-24 09:15:18 -0400

    GL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + "/"))
 
@@ -12,14 +12,34 @@ server.listen(port)
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
+app.ws('/', (wss, req) => {
+  connects.push(ws);
+
+  wss.on('message', message => {
+    console.log('Received -', message);
+    connects.forEach(socket => {
+      socket.send(message);
+    });
+  });
+  
+  wss.on('close', () => {
+    connects = connects.filter(conn => {
+      return (conn === wss) ? false : true;
     });
   });
 });
+
+
+// var wss = new WebSocketServer({server: server})
+// console.log("websocket server created")
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data);
+//       }
+//     });
+//   });
+// });
