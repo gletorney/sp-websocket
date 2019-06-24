446cd487ea209f05543d596037e88014a029b31e
Graham Letorney <gletorney@shutterstock.com>
2019-06-24 09:28:31 -0400

    GL

--- index.js
+++ index.js
@@ -15,20 +15,16 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on('connection', function connection(ws) {
-  
-  ws.on('message', message => {
-    console.log('Received -', message);
-    
-    connects.forEach(socket => {
-      socket.send(message);
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
     });
   });
-  
-  ws.on('close', () => {
+  ws.on('close', function(){
     connects = connects.filter(conn => {
       return (conn === ws) ? false : true;
     });
   });
-
 });
-
