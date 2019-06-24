d7c3bbe5c105f526be369dda608400d9665ad205
Graham Letorney <gletorney@shutterstock.com>
2019-06-24 09:32:28 -0400

    revert

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var WebSocketServer = require("ws").Server
-var https = require("https")
+var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + "/"))
 
-var server = https.createServer(app)
+var server = http.createServer(app)
 server.listen(port)
 
 console.log("http server listening on %d", port)
@@ -20,11 +20,6 @@ wss.on('connection', function connection(ws) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
-    });
-  });
-  ws.on('close', function(){
-    connects = connects.filter(conn => {
-      return (conn === ws) ? false : true;
     });
   });
 });
